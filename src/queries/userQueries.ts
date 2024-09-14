87bbd786ed2af91a7cb6e34f4a210ec8bf8de6cc
Ishmael <darmfiel@andrew.cmu.edu>
2024-09-14 05:47:10 -0400

    added query by uid functionality

--- src/queries/userQueries.ts
+++ src/queries/userQueries.ts
@@ -1,7 +1,7 @@
 // queries/userQueries.ts
 
 import { db } from "../firebase/firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, query, where, getDocs, limit} from "firebase/firestore";
 import { User } from "../models/User";
 
 const usersCollection = collection(db, "users");
@@ -12,6 +12,26 @@ export const addUserToFirestore = async (userData: User): Promise<void> => {
     console.log("User added to Firestore successfully");
   } catch (error) {
     console.error("Error adding user to Firestore:", error);
+    throw error;
+  }
+};
+
+export const queryUserByUid = async (uid: string): Promise<User | null> => {
+  try {
+    const q = query(usersCollection, where("uid", "==", uid), limit(1));
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) { 
+      return null; 
+    } 
+
+    const userDoc = querySnapshot.docs[0];
+    const userData = userDoc.data() as User;
+
+    return userData;
+
+  } catch (error) {
+    console.error("Error searching for user by UID:", error);
     throw error;
   }
 };
