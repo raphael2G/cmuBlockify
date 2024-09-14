b8fc9fe6d99f4072a18039b32efc997cc0eafa34
asilbek_omonkulov <asilbek_omonkulov>
2024-09-13 23:19:52 -0400

    created signin page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { GoogleSsoSignIn } from '@/components/google-sso-sign-in'
+import React from 'react'
+
+export const page = () => {
+  return (
+    <GoogleSsoSignIn />
+  )
+}
+
+
+export default page
