fa3d37427d422773264ac94bb00222a09f48e8c7
Ishmael <darmfiel@andrew.cmu.edu>
2024-09-14 12:16:43 -0400

    made getAllOrdersEligible query

--- src/queries/orderQueries.ts
+++ src/queries/orderQueries.ts
@@ -1,7 +1,7 @@
 // queries/orderQueries.ts
 
 import { db } from "../firebase/firebase";
-import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
+import { collection, addDoc, doc, deleteDoc, getDocs, Timestamp, query, where } from "firebase/firestore";
 import { Order } from "../models/order";
 
 const ordersCollection = collection(db, "orders");
@@ -13,6 +13,37 @@ export const addOrderToFirestore = async (orderData: Order): Promise<void> => {
   } catch (error) {
     console.error("Error adding order to Firestore:", error);
     throw error;
+  }
+};
+
+export const getAllOrders = async (): Promise<Order[]> => {
+  try {
+    // Get the current timestamp
+    const now = Timestamp.now();
+
+    // Reference to the 'orders' collection
+    const ordersRef = collection(db, "orders");
+
+    // Create a query where 'expiration' is greater than 'now'
+    const q = query(ordersRef, where("expiration", ">", now));
+
+    // Execute the query
+    const querySnapshot = await getDocs(q);
+
+    // Map the documents to the Order interface
+    const orders: Order[] = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+
+      return {
+        id: doc.id,
+        ...data
+      } as Order;
+    });
+
+    return orders;
+  } catch (error) {
+    console.error('Error fetching orders:', error);
+    throw error; // Re-throw the error after logging it
   }
 };
 
