7ebda40c64e94c7a0cdf96cd8f56aad5f3bb2256
Ishmael <darmfiel@andrew.cmu.edu>
2024-09-14 08:47:52 -0400

    updated logic to ask  questions only to new users

--- src/context/MarketContext.tsx
+++ src/context/MarketContext.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
 import { UserAuth } from "./AuthContext";
 import UserType from "@/components/user-type-selection";
+import SellerDetailsQuestionaire from "@/components/seller-details-questionaire";
+import { searchUserByUid, createUser } from "@/services/userService";
 
 interface MarketInfoContext {
 
@@ -12,10 +14,68 @@ const MarketInfoContext = createContext<MarketInfoContext>({
 
 export const MarketContextProvider = ({ children }: any) => {
     const {user, loading} = UserAuth();
-    const [showInitialScreen, setShowInitialScreen] = useState(true);
- 
+    const [showBuyerOrSellerScreen, setShowBuyerOrSellerScreen] = useState(false);
+    const [showSellerDetailsScreen, setShowSellerDetailsScreen] = useState(false);
+
+    useEffect(() => {
+        if (user){
+          searchUserByUid(user.uid).then((userQuery) => {
+            console.log("= = = = = = = = ");
+            console.log(userQuery);
+            console.log("= = = = = = = = ");
+
+            if (userQuery) {
+              setShowBuyerOrSellerScreen(false);
+              setShowSellerDetailsScreen(false);
+            } else {
+              setShowBuyerOrSellerScreen(true);
+            }
+          });
+        }
+    }, [loading]);
+
+
+
+
+
+
+    const [selectedUserType, setSelectedUserType] = useState({ buyer: false, seller: false })
+
+    const handleUserTypeToggle = (type) => {
+        setSelectedUserType((prevState) => ({
+          ...prevState,
+          [type]: !prevState[type],
+        }))
+    }
+    
+    const onBuyerOrSellerSubmit = () => {
+      setShowBuyerOrSellerScreen(false);
+      if (selectedUserType.seller) {
+        setShowSellerDetailsScreen(true);
+      }
+    };
+
+    const onSellerDetailsSubmit = () => {
+      setShowSellerDetailsScreen(false);
+      // create a user using the createUserService
+      const userToAdd = {
+        uid: user.uid,
+        andrewId: user.email.split('@')[0],
+        email: user.email,
+      }
+
+      createUser(userToAdd).then((response) => {
+        console.log(response);});
+
+    }
+
 
     const sampleUsersQueryEmpty = []
+
+
+    useEffect(() => {
+      console.log(selectedUserType);
+    }, [selectedUserType])
 
 
     if (loading) {
@@ -26,12 +86,18 @@ export const MarketContextProvider = ({ children }: any) => {
         return <div>Not logged in</div>;
     }
 
-    if (user && !loading && showInitialScreen) {
+    if (user && !loading && showBuyerOrSellerScreen) {
         return (
-            <UserType />
+          // pass selectedUserType and setSelectedUserType to UserType component
+          <UserType selectedUserType={selectedUserType} setSelectedUserType={setSelectedUserType} onBuyerOrSellerSubmit={onBuyerOrSellerSubmit}/>
         );
-        
     }
+
+    if (user && !loading && showSellerDetailsScreen) {
+      return (
+        <SellerDetailsQuestionaire onSellerDetailsSubmit={onSellerDetailsSubmit}/>
+      );
+  }
 
   return (
     <MarketInfoContext.Provider value={{}}>
